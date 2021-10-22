864b95287bb826e22d544e36fb42e5453abe64df
Gabriel Bandeira <gabrielbandeiracarneiro@gmail.com>
2021-10-22 15:41:42 -0300

    rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :parking
+  put 'parking/:id/pay', to: 'parking#pay'
+  put 'parking/:id/out', to: 'parking#out'
 end

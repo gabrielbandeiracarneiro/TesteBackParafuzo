f6c15dbd142ac132f0ccdfb0780d3c7474fff129
Gabriel Bandeira <gabrielbandeiracarneiro@gmail.com>
2021-10-22 15:39:56 -0300

    model parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,2 +1,17 @@
 class Parking < ApplicationRecord
+    #Validação do padrão de placa
+    validates :plate, presence: true, format: { with: /\A[a-zA-Z]{3}-(\d{4}|[0-9][a-zA-Z]\d{2})\z/,message: "is invalid"}
+    
+    #Validando se o veículo não encontra-se no estacionamento
+    validate :plateOnParking, on: :create
+
+    private
+    def plateOnParking
+        parkings = Parking.where(plate:self.plate,left:false).order('created_at DESC')
+        
+        #verificando se existe registros
+		if(parkings.length!=0)	
+            errors.add :plate, message: "already parked"
+		end
+    end
 end
